5921141ab504612b0d2ba0201a32a60892e2a490
Manuel Goulão <msilvagoulao@gmail.com>
2025-11-14 09:30:40 +0000

    Fix highway-env

--- _scripts/scrape_stats.py
+++ _scripts/scrape_stats.py
@@ -14,7 +14,7 @@ REPOS_URLS = "https://api.github.com/repos/Farama-Foundation/{repo}"
 REPOS_USE_URLS = "https://github.com/Farama-Foundation/{repo}/network/dependents"
 GYM_REPOS_USE_URLS = "https://github.com/openai/gym/network/dependents"
 
-SUBSUMED_PACKAGES = ["gym", "babyai", "gym-minigrid", "magent", "highway-env", "mo-gym"]
+SUBSUMED_PACKAGES = ["gym", "babyai", "gym-minigrid", "magent", "mo-gym"]
 
 
 def scrape_project_downloads(project):
